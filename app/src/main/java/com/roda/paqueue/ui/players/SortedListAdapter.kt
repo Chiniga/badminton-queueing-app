f94806812b5a64931d624a436901b3ed65543a7d
David <david.graves.2011@gmail.com>
2020-05-28 15:22:42 +0800

    Add player edit functionality

--- app/src/main/java/com/roda/paqueue/ui/players/SortedListAdapter.kt
+++ app/src/main/java/com/roda/paqueue/ui/players/SortedListAdapter.kt
@@ -1,13 +1,16 @@
 package com.roda.paqueue.ui.players
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SortedList
 import androidx.recyclerview.widget.SortedListAdapterCallback
+import com.google.android.material.textfield.TextInputLayout
 import com.roda.paqueue.R
 import com.roda.paqueue.models.Player
 import io.realm.Realm
@@ -47,29 +50,56 @@ class SortedListAdapter : RecyclerView.Adapter<SortedListAdapter.UserViewHolder>
         }
 
         holder.textViewOptions.setOnClickListener {
-            val popup = PopupMenu(holder.textViewPlayerName.context, holder.itemView)
+            val popup = PopupMenu(it.context, it)
             popup.inflate(R.menu.player_row_menu)
 
             popup.setOnMenuItemClickListener { item ->
                 when(item.itemId) {
                     R.id.itmEditPlayer ->
-                        Toast.makeText(holder.itemView.context, "EDIT", Toast.LENGTH_LONG).show()
+                        holder.enableEdit(playerSortedList.get(holder.adapterPosition))
                 }
                 true
             }
 
             popup.show()
         }
+
+        holder.imgBtnDoneEditing.setOnClickListener {
+            val currPos = holder.adapterPosition
+            val player = playerSortedList.get(currPos)
+            val newPlayerName = holder.editTextEditPlayer.text
+            val newPlayerLevel = holder.ratingBarLevel.rating
+            if(player.isValidName(newPlayerName.toString()) && newPlayerLevel != 0.0f) {
+                holder.textViewPlayerName.visibility = View.VISIBLE
+                holder.textViewOptions.visibility = View.VISIBLE
+                holder.ratingBarLevel.setIsIndicator(true)
+                holder.textInputLayout.visibility = View.GONE
+                holder.imgBtnDoneEditing.visibility = View.GONE
+                Realm.getDefaultInstance().use { realm ->
+                    realm.executeTransaction { r ->
+                        player.name = newPlayerName.toString()
+                        player.level = newPlayerLevel
+                        editPlayer(player, currPos)
+                        r.insertOrUpdate(player)
+                    }
+                }
+
+                val softKeyboard: InputMethodManager = holder.itemView.rootView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                if(softKeyboard.isActive) {
+                    softKeyboard.hideSoftInputFromWindow(holder.itemView.rootView.windowToken, 0)
+                }
+            }
+        }
     }
 
     override fun getItemCount() = playerSortedList.size()
 
-    fun addPlayer(player: Player) {
-        playerSortedList.add(player)
-    }
-
     fun addPlayers(player: List<Player>) {
         playerSortedList.addAll(player)
+    }
+
+    private fun editPlayer(player: Player, index: Int) {
+        playerSortedList.updateItemAt(index, player)
     }
 
     private fun removePlayer(index: Int) {
@@ -85,10 +115,26 @@ class SortedListAdapter : RecyclerView.Adapter<SortedListAdapter.UserViewHolder>
         var ratingBarLevel: RatingBar = itemView.findViewById(R.id.ratingBarLevel)
         var btnDeletePlayer: ImageButton = itemView.findViewById(R.id.btnDeletePlayer)
         var textViewOptions: TextView = itemView.findViewById(R.id.textViewOptions)
+        var imgBtnDoneEditing: ImageButton = itemView.findViewById(R.id.btnDoneEditing)
+        var textInputLayout: TextInputLayout = itemView.findViewById(R.id.textInputLayout)
+        var editTextEditPlayer: EditText = itemView.findViewById(R.id.editTextEditPlayerName)
 
         fun setPlayer(player: Player) {
             textViewPlayerName.text = player.name
             ratingBarLevel.rating = player.level
+        }
+
+        fun enableEdit(player: Player) {
+            textViewPlayerName.visibility = View.GONE
+            textViewOptions.visibility = View.GONE
+            ratingBarLevel.setIsIndicator(false)
+            textInputLayout.visibility = View.VISIBLE
+            imgBtnDoneEditing.visibility = View.VISIBLE
+            editTextEditPlayer.setText(player.name)
+            editTextEditPlayer.requestFocus()
+
+            val softKeyboard: InputMethodManager = itemView.rootView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            softKeyboard.showSoftInput(itemView.rootView, InputMethodManager.SHOW_FORCED)
         }
     }
 }
