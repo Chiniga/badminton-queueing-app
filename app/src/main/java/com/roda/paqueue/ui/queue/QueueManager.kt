c6b917fd9e1b45c88eb44fb18e08c2a247ed90c0
David <david.graves.2011@gmail.com>
2020-06-26 23:27:24 +0800

    Generate queues by player amount and fix lower_tier and upper_tier issues

--- app/src/main/java/com/roda/paqueue/ui/queue/QueueManager.kt
+++ app/src/main/java/com/roda/paqueue/ui/queue/QueueManager.kt
@@ -1,5 +1,7 @@
 package com.roda.paqueue.ui.queue
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.util.Range
 import com.roda.paqueue.models.Player
 import com.roda.paqueue.models.Queue
@@ -9,6 +11,7 @@ import io.realm.kotlin.oneOf
 import io.realm.kotlin.where
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.ceil
 
 class QueueManager(private val realm: Realm) {
 
@@ -17,19 +20,22 @@ class QueueManager(private val realm: Realm) {
     private val UPPER_TIER: IntRange = 5..6
 
     fun generate(courts: Int) {
-        TODO("generate queues based on num_players/4 instead of num_courts")
-        for(court in 1..courts) {
+        val allPlayers = realm.where<Player>().findAll()
+        val queueCount = ceil(allPlayers.size.toDouble() / 4.0).toInt()
+        for(count in 1..queueCount) {
             realm.executeTransaction {
                 // get all players with less or no queues
                 val playerList = getPlayers()
 
                 // check queue for active queue with specific court number
-                val checkQueue = realm.where<Queue>().equalTo("court_number", court)
-                    .equalTo("status", QueueConstants.STATUS_ACTIVE).findFirst()
+                val activeQueues = realm.where<Queue>().equalTo("status", QueueConstants.STATUS_ACTIVE)
+                    .findAll()
+
+                val court = if(activeQueues.isEmpty()) 1 else activeQueues.size + 1
 
                 // add players to queue
                 val queue = realm.createObject(Queue::class.java, UUID.randomUUID().toString())
-                if(checkQueue == null) {
+                if(court <= courts) {
                     queue.status = QueueConstants.STATUS_ACTIVE
                     queue.court_number = court
                 }
@@ -68,11 +74,12 @@ class QueueManager(private val realm: Realm) {
                 // 1-2 = opponents with 3 or less total level
                 var otherLevelTotal = 0
                 val maxLevel = 2.0f
+                val maxTotal = if(levelTotal == 2) 4 else 3
                 twoMorePlayers = realm.where<Player>().lessThanOrEqualTo("level", maxLevel)
                     .not().oneOf("id", excludePlayerIds.toTypedArray())
                     .sort("queues_games").findAll()
                 for(player in twoMorePlayers) {
-                    if((otherLevelTotal + player.level.toInt()) <= 3) {
+                    if((otherLevelTotal + player.level.toInt()) <= maxTotal) {
                         otherLevelTotal += player.level.toInt()
                         list.add(player)
                     }
@@ -102,12 +109,14 @@ class QueueManager(private val realm: Realm) {
                 // 3-3 = opponents with 5-6 total level
                 var otherLevelTotal = 0
                 val minLevel = 2.0f
+                val minTotal = if(levelTotal == 6) 4 else 5
                 twoMorePlayers = realm.where<Player>().greaterThanOrEqualTo("level", minLevel)
                     .not().oneOf("id", excludePlayerIds.toTypedArray())
                     .sort("queues_games").findAll()
                 for(player in twoMorePlayers) {
                     if((list.size == 2) ||
-                        ((otherLevelTotal + player.level.toInt()) <= 6)) {
+                        (((otherLevelTotal + player.level.toInt()) <= 6) &&
+                                ((otherLevelTotal + player.level.toInt()) >= minTotal))) {
                         otherLevelTotal += player.level.toInt()
                         list.add(player)
                     }
