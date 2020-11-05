a0df2848b48698e0881790b8c5f151cb2dd20501
David <david.graves.2011@gmail.com>
2020-11-05 11:36:45 +0800

    Add Ball Calculator activity

---
+++ app/src/main/java/com/roda/paqueue/BallCalculatorActivity.kt
@@ -0,0 +1,11 @@
+package com.roda.paqueue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BallCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ball_calculator)
+    }
+}
