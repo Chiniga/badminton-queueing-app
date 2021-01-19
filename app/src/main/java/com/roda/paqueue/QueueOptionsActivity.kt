c727573eb4dfc4a94762069eebeafc71faa3b5b8
David <david.graves.2011@gmail.com>
2021-01-19 14:57:18 +0800

    Add descriptions per queue style

--- app/src/main/java/com/roda/paqueue/QueueOptionsActivity.kt
+++ app/src/main/java/com/roda/paqueue/QueueOptionsActivity.kt
@@ -2,25 +2,37 @@ package com.roda.paqueue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.SeekBar
+import android.widget.TextView
 
 class QueueOptionsActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+    private lateinit var queueStyleDescArray: Array<TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_queue_options)
         val seekBar = findViewById<SeekBar>(R.id.seekBarQueueStyle)
         seekBar.setOnSeekBarChangeListener(this)
+        queueStyleDescArray = arrayOf(
+            findViewById(R.id.textViewOpenDesc),
+            findViewById(R.id.textViewByLevelDesc),
+            findViewById(R.id.textViewMixedDesc)
+        )
     }
 
-    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-        TODO("Not yet implemented")
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        for(desc in queueStyleDescArray) {
+            desc.visibility = View.GONE
+        }
+        queueStyleDescArray[progress].visibility = View.VISIBLE
     }
 
     override fun onStartTrackingTouch(p0: SeekBar?) {
-        TODO("Not yet implemented")
+
     }
 
     override fun onStopTrackingTouch(p0: SeekBar?) {
-        TODO("Not yet implemented")
+
     }
 }
